28cccb266be7c319224a5f690e0de9ba34786643
Nedim <nedim@nedim-VirtualBox.(none)>
2013-12-22 01:06:19 +0100

    Superadmin dio odradjen. Ostalo jos dodavanje radnika za centre.

--- Centar/urls.py
+++ Centar/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^user/(?P<u_id>\d+)/$', views.user, name='user'),
     url(r'^teren/(?P<t_id>\d+)/$', views.teren, name='teren'),
     url(r'^out/$', views.out, name='out'),
+    url(r'^superadmin/(?P<u_id>\d+)/$', views.superadmin, name='superadmin'),
 )
